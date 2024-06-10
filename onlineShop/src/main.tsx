a2d1b872f87ddf162134fe382318d992c933bf30
Alexey Katnikov <88578354+KatnikovAA@users.noreply.github.com>
2024-06-10 21:55:07 +0300

    instal redux

--- onlineShop/src/main.tsx
+++ onlineShop/src/main.tsx
@@ -4,6 +4,7 @@ import App from './components/App/App.tsx'
 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
