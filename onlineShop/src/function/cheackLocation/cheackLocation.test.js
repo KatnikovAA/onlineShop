3b720b93a0d637b607651acab6e5ba9727ab6e91
Alexey Katnikov <88578354+KatnikovAA@users.noreply.github.com>
2024-06-23 11:31:34 +0300

    done task3_3

--- onlineShop/src/function/cheackLocation/cheackLocation.test.js
+++ onlineShop/src/function/cheackLocation/cheackLocation.test.js
@@ -1,12 +1,12 @@
 import {cheackLocation} from './cheackLocation';
 
 describe("cheackLocation", () => {
-    it("should return a string of length 23 when input string has more than 20 characters", () => {
+    test("should return true when input /", () => {
       const inputString = "/";
       expect(cheackLocation(inputString)).toBe(true);
     });
   
-    it("should return a string of length 23 when input string has more than 20 characters", () => {
+    test("should return false when input /auth", () => {
         const inputString = "/auth";
         expect(cheackLocation(inputString)).toBe(false);
       });
