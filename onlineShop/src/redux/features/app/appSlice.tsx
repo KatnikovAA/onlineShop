cce59d98b653d581fd9a917474be8f739f1498c7
Alexey Katnikov <88578354+KatnikovAA@users.noreply.github.com>
2024-06-23 11:51:17 +0300

    done task3_4

--- onlineShop/src/redux/features/app/appSlice.tsx
+++ onlineShop/src/redux/features/app/appSlice.tsx
@@ -32,86 +32,8 @@ export const dataCartUserSlice = createSlice({
     deleteProduct: (state, id: PayloadAction<number>) => {
 
       state.dataCart.carts[0].products = state.dataCart.carts[0].products.filter(product => product.id !== id.payload);
-      // if(data.payload.products[0].quantity !== 0 ){
-      //   state.dataCart.carts[0].discountedTotal = state.dataCart.carts[0].discountedTotal - (data.payload.products[0].discountedPrice/data.payload.products[0].quantity) * data.payload.products[0].quantity 
-      //   state.dataCart.carts[0].totalQuantity = state.dataCart.carts[0].totalQuantity - data.payload.products[0].quantity;
-      //   state.dataCart.carts[0].total = state.dataCart.carts[0].total - (data.payload.products[0].price * data.payload.products[0].quantity)
-      // }
 
     },
-    // plusQuantity: (state, data: PayloadAction<apiCarts>) => {
-    //   const productIndex = state.dataCart.carts[0].products.findIndex(
-    //     product => product.id === data.payload.products[0].id
-    //   );
-    //   const productPrice = data.payload.products[0].price;
-    //   const productDiscount = data.payload.products[0].discountPercentage;
-    //   state.dataCart.carts[0].totalQuantity = state.dataCart.carts[0].totalQuantity + 1;
-    //   state.dataCart.carts[0].total = state.dataCart.carts[0].total + data.payload.products[0].price
-    //   state.dataCart.carts[0].discountedTotal = state.dataCart.carts[0].discountedTotal + (productPrice - (productPrice * productDiscount / 100))
-
-    //   if (productIndex !== -1) {
-    //       // Продукт уже есть, заменяем его
-    //       state.dataCart.carts[0].products[productIndex] = data.payload.products[0];
-    //   } else {
-    //       // Продукта нет, добавляем его
-    //       state.dataCart.carts[0].products.push(data.payload.products[0]);
-    //   }
-    // },
-    // minusQuantity: (state, data: PayloadAction<apiCarts>) => {
-    //   const productIndex = state.dataCart.carts[0].products.findIndex(
-    //     product => product.id === data.payload.products[0].id
-    //   );
-    //     const productPrice = data.payload.products[0].price;
-    //     const productDiscount = data.payload.products[0].discountPercentage;
-    //     state.dataCart.carts[0].totalQuantity = state.dataCart.carts[0].totalQuantity - 1;
-    //     state.dataCart.carts[0].total = state.dataCart.carts[0].total - data.payload.products[0].price
-    //     state.dataCart.carts[0].discountedTotal = state.dataCart.carts[0].discountedTotal - (productPrice - (productPrice * productDiscount / 100))
-  
-      
-
-    //   if (productIndex !== -1) {
-    //       // Продукт уже есть, заменяем его
-    //       state.dataCart.carts[0].products[productIndex] = data.payload.products[0];
-    //   } else {
-    //       // Продукта нет, добавляем его
-    //       state.dataCart.carts[0].products.push(data.payload.products[0]);
-    //   }
-    // },
-    // minusProductQuantity: (state, id: PayloadAction<number>) => {
-    //   const productIndex = state.dataCart.carts[0].products.findIndex(product => product.id === id.payload);
-    //   if (productIndex !== -1 && state.dataCart.carts[0].products[productIndex].quantity > 0) {
-    //     const productPrice = state.dataCart.carts[0].products[productIndex].price;
-    //     const productDiscount = state.dataCart.carts[0].products[productIndex].discountPercentage;
-    //     const discountedPrice = productPrice - (productPrice * productDiscount / 100);
-    //     state.dataCart.carts[0].products[productIndex].quantity = state.dataCart.carts[0].products[productIndex].quantity - 1
-    //     state.dataCart.carts[0].totalQuantity = state.dataCart.carts[0].totalQuantity - 1
-    //     state.dataCart.carts[0].discountedTotal = parseFloat((state.dataCart.carts[0].discountedTotal - discountedPrice).toFixed(10));
-    //     state.dataCart.carts[0].total = state.dataCart.carts[0].total - state.dataCart.carts[0].products[productIndex].price
-    //   }
-    // },
-    // addProduct: (state, newPordcut: PayloadAction<apiProducts>) => {
-      //state.dataCart.carts[0].products = state.dataCart.carts[0].products.push(newPordcut.payload)
-      // const productIndex = state.dataCart.carts[0].products.findIndex(product => product.id === id.payload);
-      // if (productIndex !== -1 && state.dataCart.carts[0].products[productIndex].quantity > 0) {
-      //   const productPrice = state.dataCart.carts[0].products[productIndex].price;
-      //   const productDiscount = state.dataCart.carts[0].products[productIndex].discountPercentage;
-      //   const discountedPrice = productPrice - (productPrice * productDiscount / 100);
-      //   state.dataCart.carts[0].products = state.dataCart.carts[0].products.push(newPordcut.payload)
-      //   state.dataCart.carts[0].products[productIndex].quantity = state.dataCart.carts[0].products[productIndex].quantity - 1
-      //   state.dataCart.carts[0].totalQuantity = state.dataCart.carts[0].totalQuantity - 1
-      //   state.dataCart.carts[0].discountedTotal = parseFloat((state.dataCart.carts[0].discountedTotal - discountedPrice).toFixed(10));
-      //   state.dataCart.carts[0].total = state.dataCart.carts[0].total - state.dataCart.carts[0].products[productIndex].price
-      // }
-    // },
-      
-      // const productIndex = state.dataCart.carts[0].products.findIndex(product => product.id === id.payload);
-      // const countPrice = state.dataCart.carts[0].products[productIndex].quantity * state.dataCart.carts[0].products[productIndex].price
-      // if (productIndex !== -1) {
-      //   state.dataCart.carts[0].totalQuantity = state.dataCart.carts[0].totalQuantity - state.dataCart.carts[0].products[productIndex].quantity
-      //   state.dataCart.carts[0].discountedTotal = parseFloat((state.dataCart.carts[0].discountedTotal - (countPrice - (countPrice * state.dataCart.carts[0].products[productIndex].discountPercentage/100))).toFixed(2))        
-      //   state.dataCart.carts[0].total = state.dataCart.carts[0].total - countPrice
-      //   state.dataCart.carts[0].products = state.dataCart.carts[0].products.filter(product => product.id !== id.payload);
-      // }
     
   },
 })
